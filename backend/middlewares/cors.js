463bdb89b02a53e2cb81cf54d3d899cf4e859e51
Valentina Lugovaia <vaal.it@yandex.ru>
2022-10-30 16:02:34 +0300

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,19 +1,11 @@
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-];
-
 const cors = (req, res, next) => {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Origin', '*');
-
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', true);
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
@@ -21,6 +13,7 @@ const cors = (req, res, next) => {
 
     return res.end();
   }
+
   return next();
 };
 
